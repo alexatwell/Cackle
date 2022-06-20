681c8c04c72114ccc9e92d73cb6233b1bfb86181
alexatwell <alexatwell246@gmail.com>
2022-06-20 03:27:48 -0400

    Commit #15
    Home Page Images Added
    Profile Page Images Added

--- public/contact/mail.php
+++ public/contact/mail.php
@@ -1,35 +1,32 @@
 <?php
-    $Submit = isset($_POST['Submit']) ? $_POST['Submit'] : false;
-    if($Submit){
-        echo bruh;
+    $email = "floodtechbdos@gmail.com";
+    $name = $_POST['name'];
+    $email2 = "Reply To: " . $_POST['email'] . "\n\n";
+    $subject = $name . " : " . $_POST['subject'];
+    $message = $email2 . $_POST['message'];
+
+    $url = "https://script.google.com/macros/s/AKfycbxnIYwhsGbFIkV7uTRZprqefDXr0OktaMkNo8wjZy153yJhuYB2rNROZrGHHQf0ycgrZQ/exec";
+
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+       CURLOPT_RETURNTRANSFER => true,
+       CURLOPT_FOLLOWLOCATION => true,
+       CURLOPT_POSTFIELDS => http_build_query([
+          "recipient" => $email,
+          "subject"   => $subject,
+          "body"      => $message
+       ])
+    ]);
+
+    $result = curl_exec($ch);
+
+    if($result){
+        echo '<script>alert("Successful Submission")</script>';
     }else{
-        $email = "floodtechbdos@gmail.com";
-        $name = $_POST['name'];
-        $email2 = "From: " . $_POST['email'] . "\n\n";
-        $subject = $_POST['subject'];
-        $message = $email2 . $_POST['message'];
-
-        $url = "https://script.google.com/macros/s/AKfycbxnIYwhsGbFIkV7uTRZprqefDXr0OktaMkNo8wjZy153yJhuYB2rNROZrGHHQf0ycgrZQ/exec";
-
-        $ch = curl_init($url);
-        curl_setopt_array($ch, [
-           CURLOPT_RETURNTRANSFER => true,
-           CURLOPT_FOLLOWLOCATION => true,
-           CURLOPT_POSTFIELDS => http_build_query([
-              "recipient" => $email,
-              "subject"   => $subject,
-              "body"      => $message
-           ])
-        ]);
-
-        $result = curl_exec($ch);
-
-        if($result){
-            alert("Successful Submission");
-            print "Successful Submission";
-        }else{
-            alert("Unsuccessful Submission");
-            print "Unsuccessful Submission";
-        }
+        echo '<script>alert("Unsuccessful Submission")</script>';
     }
 ?>
+
+<script>
+window.location.href="contact.html"
+</script>
